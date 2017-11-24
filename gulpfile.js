64fec19e297d7ddcdd51121fe15f3c802b9e5965
JohnsonMauro <johnsonmauro@gmail.com>
2017-11-24 17:45:05 -0300

    montar base do gulpfile close #5

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp'),
+  pug = require('gulp-pug'),
+  stylus = require('gulp-stylus'),
+  connect = require('gulp-connect');
+
+gulp.task('pug', () => gulp.src('./src/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./out'))
+  .pipe(connect.reload()))
+
+gulp.task('stylus', () => gulp.src('./src/assets/styles/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('./out/assets/styles/'))
+  .pipe(connect.reload()))
+
+gulp.task('watch', () => {
+  gulp.watch(['./src/*.pug'], ['pug'])
+  gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
+})
+
+gulp.task('serve', () => connect.server({
+  root: './out',
+  livereload: true
+}))
+
+gulp.task('build', ['pug', 'stylus'])
+gulp.task('server', ['serve', 'watch'])
