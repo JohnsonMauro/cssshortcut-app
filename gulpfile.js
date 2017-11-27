bd3fb184d0b1cfa7199145594fe2056ff98a147e
Johnson Mauro <johnsonmauro@gmail.com>
2017-11-27 00:15:30 -0300

    Live Code - Aula Header responsivo com Flexbox-Parte1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
   pug = require('gulp-pug'),
   stylus = require('gulp-stylus'),
+  imagemin = require('gulp-imagemin'),
   connect = require('gulp-connect');
 
 gulp.task('pug', () => gulp.src('./src/*.pug')
@@ -13,9 +14,21 @@ gulp.task('stylus', () => gulp.src('./src/assets/styles/*.styl')
   .pipe(gulp.dest('./out/assets/styles/'))
   .pipe(connect.reload()))
 
+gulp.task('imagemin', () =>
+  gulp.src('src/assets/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('out/assets/img/'))
+);
+
+// gulp.task('tinify', () =>
+//   gulp.src('src/assets/img/*')
+//   .pipe(tinify('f5Bp9L5AjZa4bda9Bq9k4JyhYMhZOtIw'))
+//   .pipe(gulp.dest('/out/assets/img/')))
+
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.pug'], ['pug'])
+  gulp.watch(['./src/**/*.pug'], ['pug'])
   gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
+  gulp.watch(['./src/assets/scripts/*.js'])
 })
 
 gulp.task('serve', () => connect.server({
